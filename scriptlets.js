e2f14adc241f865156634f21a8670dd5a1779d32
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-23 20:51:01 -0400

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -3,7 +3,7 @@
 /// this is from https://github.com/uBlock-user/uBO-Scriptlets/commit/3d1f48573749ac85b20031f78e0d5f7c7bb0f3af#
 /// cookie-set.js
 /// alias cs.js
-// example.com##+js(cs, name, value, age)
+// example.com##+js(clog, data)
 (() => {
 		'use strict';
 		const cs = ev => {
