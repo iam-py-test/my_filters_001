6bea2d9a0b2e3ba8cba361dce886e4f8cf7469b8
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-26 09:47:47 -0400

    Find duplicates

--- update.py
+++ update.py
@@ -1,11 +1,16 @@
 import os
 lines = open("antimalware.txt").read().split("\n")
 alt = open("Alternative list formats/antimalware_domains.txt","w")
+donedomains = []
 for line in lines:
   if line.startswith("||") and "^" in line:
     domain = line.split("$")[0][2:-1]
-    alt.write("{}\n".format(domain))
+    if domain not in donedomains:
+       alt.write("{}\n".format(domain))
+       donedomains.append(domain)
     continue
   if line == '' or line.startswith("!") or line.startswith("||"):
     continue
-  alt.write("{}\n".format(line.split("$")[0]))
+  if line.split("$")[0] not in donedomains:
+    alt.write("{}\n".format(line.split("$")[0]))
+    donedomains.append(line.split("$")[0])
