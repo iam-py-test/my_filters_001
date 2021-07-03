ec3c22a6022d8df0aec67cde08b8eade1c9ce4fe
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-07-03 18:45:29 -0400

    Fix https://github.com/iam-py-test/my_filters_001/issues/80

--- update.py
+++ update.py
@@ -2,7 +2,16 @@
 def mkalt(file,alt):
   lines = open(file).read().split("\n")
   alt = open("Alternative list formats/{}".format(alt),"w")
+  iponly = open("Alternative list formats/{}_ips.txt".format(file.split(".")[0]),"w")
   donedomains = []
+  def isipdomain(domain):
+    try:
+      import socket
+      if socket.gethostbyname(domain) == domain:
+        return True
+     except:
+      pass
+    return False
   for line in lines:
     if line.startswith("||") and "^" in line:
       domain = line.split("$")[0][2:-1]
@@ -10,11 +19,15 @@ def mkalt(file,alt):
         alt.write("{}\n".format(domain))
         donedomains.append(domain)
       continue
+    if isipdomain(line.split("$")[0] == True):
+      iponly.write(line.split("$")[0] + "\n")
+      continue
     if line == '' or line.startswith("!") or line.startswith("||") or line == '[Adblock Plus 2.0]':
       continue
     if line.split("$")[0] not in donedomains:
       alt.write("{}\n".format(line.split("$")[0]))
       donedomains.append(line.split("$")[0])
+    iponly.close()
 mkalt("antimalware.txt","antimalware_domains.txt")
 mkalt("porn.txt","porn_domains.txt")
 mkalt("antitypo.txt","antitypo_domains.txt")
