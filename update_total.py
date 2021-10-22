35415a2c6509ea36c49c9217479c5f0543eb6c2f
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-10-22 08:14:54 -0400

    More lists and per-list stats

--- update_total.py
+++ update_total.py
@@ -1,6 +1,7 @@
 num = 0
-lists = ["porn.txt","antimalware.txt","antitypo.txt","anti-redirectors.txt","anti-cookie+sign up.txt","anti-rickroll-list.txt","annoyances.txt","duckduckgo-clean-up.template","enhanced_protection.txt"]
+lists = ["porn.txt","antimalware.txt","antitypo.txt","anti-redirectors.txt","anti-cookie+sign up.txt","anti-rickroll-list.txt","annoyances.txt","duckduckgo-clean-up.template","enhanced_protection.txt","anti-norton-tracking.txt","clickbait.txt"]
 reviewedlines = {}
+perlistnum = {}
 for list in lists:
   reviewedlines[list] = []
   lines = open(list).read().split('\n')
@@ -15,3 +16,10 @@ allentries = """<svg height="20" width="130" xmlns="http://www.w3.org/2000/svg" 
 totalentries = open("totalentries.svg","w")
 totalentries.write(allentries.format(num))
 totalentries.close()
+
+stats = open("stats.md","w")
+stats.write("## List stats\n")
+for list1 in reviewedlines:
+  stats.write("#### {}\n".format(list1))
+  stats.write("Entries: {} <br> \n".format(len(reviewedlines[list1])))
+stats.close()
