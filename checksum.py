9c216e21e9706683015ee25e43c3783dce32791f
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-11-05 18:01:07 -0400

    Generate the checksums for other files

--- checksum.py
+++ checksum.py
@@ -1,11 +1,11 @@
 """Generate checksums for various lists"""
 from hashlib import sha512
-lists = ["antimalware.txt","Alternative list formats/antimalware_domains.txt","clickbait.txt","annoyances.txt","porn.txt","antitypo.txt"]
+lists = ["antimalware.txt","Alternative list formats/antimalware_domains.txt","clickbait.txt","annoyances.txt","porn.txt","antitypo.txt","anti-redirectors.txt","duckduckgo-clean-up.txt","special_lists/google-safe-browsing-reverse-engineered.txt","special_lists/anti-malware-ubo-extension.txt"]
 checksums = {}
 for listi in lists:
   checksums[listi] = sha512(open(listi,"rb").read()).hexdigest()
 
 checksumf = open("checksums.txt","w")
-for item in checksums:
+for item in checksums:special_lists/anti-malware-ubo-extension.txt
   checksumf.write("{}: {}\n".format(item,checksums[item]))
 checksumf.close()
