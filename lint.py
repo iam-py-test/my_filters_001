e6277c636e4136a5a522f617d96f109bfa715243
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-11-26 07:43:14 -0500

    Add new domains to allowlist

--- lint.py
+++ lint.py
@@ -1,15 +1,15 @@
 """Verify the syntax of my antimalware list and verify there are no legit domains in it"""
 
 # domains which are good & should never be blocked in this list
-gooddomains = ["google.com","www.google.com","duckduckgo.com","www.duckduckgo.com","virustotal.com","safeweb.norton.com","mywot.com","www-amazon-com.customer.fastly.net","fastly.net","adguardteam.github.io","iam-py-test.github.io","example.com","r3.o.lencr.org","mozilla.org","www.mozilla.org","www.mozorg.moz.works","addons.mozilla.org","github.community"]
+gooddomains = ["google.com","www.google.com","duckduckgo.com","www.duckduckgo.com","virustotal.com","safeweb.norton.com","mywot.com","www-amazon-com.customer.fastly.net","adguardteam.github.io","iam-py-test.github.io","example.com","r3.o.lencr.org","mozilla.org","www.mozilla.org","www.mozorg.moz.works","github.community","somepythonthings.tk","letsencrypt.org","easylist.to","ublockorigin.com","microsoft.com"]
 # domains which are used for hosting or contain User Generated Content, and should only have subdomains/specific urls listed
-hosting = ["duckdns.org","appspot.com","blogspot.com","raw.githubusercontent.com","github.com","gitlab.com","github.io","storage.cloud.google.com","mediafire.com","archive.org"]
+hosting = ["duckdns.org","appspot.com","blogspot.com","raw.githubusercontent.com","github.com","gitlab.com","github.io","storage.cloud.google.com","mediafire.com","archive.org","fastly.net","addons.mozilla.org","sites.google.com"]
 # social media and email
-social = ["reddit.com","twitter.com","slack.com","meet.google.com","mail.google.com","gmail.com","chat.google.com","discord.gift","discord.com"]
+social = ["reddit.com","twitter.com","slack.com","meet.google.com","mail.google.com","gmail.com","chat.google.com","discord.gift","discord.com","protonmail.com","outlook.com","facebook.com","old.reddit.com","slack.com"]
 # url shorteners which should only have specific urls blocked
-urlshorteners = ["bit.ly","x.co","tinyurl.com","t.co","t.ly","sites.google.com","urldefense.proofpoint.com"]
-# invalid syntax in uBlock Origin
-invalidsyntax = ["$$","$docment","$alll","^all","$docs","$scripted","|||","$alls","$documentall","$allall","$all$all","$all.","$docments"]
+urlshorteners = ["bit.ly","x.co","tinyurl.com","t.co","t.ly","urldefense.proofpoint.com"]
+# invalid syntax
+invalidsyntax = ["$$","$docment","$alll","^all","$docs","$scripted","|||","$alls","$documentall","$allall","$all$all","$all.","$docments","$doc$doc"]
 
 # the main text
 maintext = open("antimalware.txt").read()
