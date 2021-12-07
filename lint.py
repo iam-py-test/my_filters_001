13a97b797912522eb53beed09d2f0adee0510bc8
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-12-07 07:14:32 -0500

    Update lint.py

--- lint.py
+++ lint.py
@@ -3,7 +3,7 @@
 # domains which are good & should never be blocked in this list
 gooddomains = ["google.com","www.google.com","duckduckgo.com","www.duckduckgo.com","virustotal.com","safeweb.norton.com","mywot.com","www-amazon-com.customer.fastly.net","adguardteam.github.io","iam-py-test.github.io","example.com","r3.o.lencr.org","mozilla.org","www.mozilla.org","www.mozorg.moz.works","github.community","somepythonthings.tk","letsencrypt.org","easylist.to","ublockorigin.com","microsoft.com","amazon.com","mcafee.com","norton.com"]
 # domains which are used for hosting or contain User Generated Content, and should only have subdomains/specific urls listed
-hosting = ["duckdns.org","appspot.com","blogspot.com","raw.githubusercontent.com","github.com","gitlab.com","github.io","storage.cloud.google.com","mediafire.com","archive.org","fastly.net","addons.mozilla.org","sites.google.com","ips-cic-filestore.s3.amazonaws.com","s3.amazonaws.com","amazonaws.com"]
+hosting = ["duckdns.org","appspot.com","blogspot.com","raw.githubusercontent.com","github.com","gitlab.com","github.io","storage.cloud.google.com","mediafire.com","archive.org","fastly.net","addons.mozilla.org","sites.google.com","ips-cic-filestore.s3.amazonaws.com","s3.amazonaws.com","amazonaws.com","pp.ua"]
 # social media and email
 social = ["reddit.com","twitter.com","slack.com","meet.google.com","mail.google.com","gmail.com","chat.google.com","discord.gift","discord.com","protonmail.com","outlook.com","facebook.com","old.reddit.com","slack.com","youtube.com","www.youtube.com","youtubekids.com"]
 # url shorteners which should only have specific urls blocked
