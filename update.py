28296e99ca8758ef5cb1ba7c0cdc9ec47bcff8a4
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-05-06 07:16:01 -0400

    Update update.py

--- update.py
+++ update.py
@@ -281,7 +281,7 @@ def mkps_firewall_block(file,outfile):
     # safety check to make sure this doesn't turn into a prefect RCE
     if "\"" not in ip and ";" not in ip and "-" not in ip and ":/" not in ip:
       outf.write("New-NetFirewallRule -DisplayName \"iam-py-test - Block outbound connections to this ip\" -Direction outbound -LocalPort Any -Protocol tcp -Action Block -RemoteAddress {}\nNew-NetFirewallRule -DisplayName \"iam-py-test - Block inbound connections from this ip\" -Direction Inbound -LocalPort Any -Protocol tcp -Action Block -RemoteAddress {}\n".format(ip,ip))
-  outf.write("\n\necho All rules should have been added to the Windows Firewall\npause\n")
+  outf.write("\n\necho \"All rules should have been added to the Windows Firewall\"\npause\n")
   outf.close()
 try:
   mkps_firewall_block("antimalware.txt","Alternative list formats/antimalware_firewall_script.ps1")
