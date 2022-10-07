9e2fefbb278dbc8f0eea6f65842bc6c13ef86880
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-07 14:09:14 -0400

    Doesn't really work

--- detectparked.py
+++
@@ -1,8 +0,0 @@
-import requests
-def checkdomain(domain):
-  req = requests.get("http://{}".format(domain))
-  parkedpattern = ["www.afternic.com/forsale"]
-  for pattern in parkedpattern:
-    if pattern in req.url:
-      return True
-  return False
