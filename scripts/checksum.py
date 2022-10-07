564bc165e9517e91c155c421bbe39e95170a0d71
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-07 14:06:51 -0400

    Move and switch to SHA256

--- scripts/checksum.py
+++ scripts/checksum.py
@@ -1,13 +1,13 @@
 """Generate checksums for various lists"""
-from hashlib import sha512
+from hashlib import sha256
 import os
 lists = ["antimalware.txt","clickbait.txt","annoyances.txt","porn.txt","antitypo.txt","anti-redirectors.txt","duckduckgo-clean-up.txt","special_lists/google-safe-browsing-reverse-engineered.txt","special_lists/anti-malware-ubo-extension.txt"]
 checksums = {}
 for listi in lists:
-  checksums[listi] = sha512(open(listi,"rb").read()).hexdigest()
+  checksums[listi] = sha256(open(listi,"rb").read()).hexdigest()
 for root,dirs,files in os.walk("Alternative list formats"):
   for file in files:
-    checksums[os.path.join(root,file)] = sha512(open(os.path.join(root,file),"rb").read()).hexdigest()
+    checksums[os.path.join(root,file)] = sha256(open(os.path.join(root,file),"rb").read()).hexdigest()
 
 checksumf = open("checksums.txt","w")
 for item in checksums:
