5fd953b4e6711fa7afcbc0f2b921616589e81fae
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-13 07:41:45 -0500

    Speed

--- scripts/checksum.py
+++ scripts/checksum.py
@@ -1,13 +1,10 @@
 """Generate checksums for various lists"""
 from hashlib import sha256
 import os
-lists = ["antimalware.txt","clickbait.txt","annoyances.txt","porn.txt","antitypo.txt","anti-redirectors.txt","duckduckgo-clean-up.txt","special_lists/google-safe-browsing-reverse-engineered.txt","special_lists/anti-malware-ubo-extension.txt"]
+lists = ["antimalware.txt","clickbait.txt","annoyances.txt","antitypo.txt","anti-redirectors.txt","duckduckgo-clean-up.txt","special_lists/google-safe-browsing-reverse-engineered.txt","special_lists/anti-malware-ubo-extension.txt","Alternative list formats/antimalware_domains.txt","Alternative list formats/antimalware_hosts.txt","Alternative list formats/antimalware_abp.txt","Alternative list formats/antimalware_adguard_app.txt","Alternative list formats/antimalware_adguard_home.txt","Alternative list formats/antimalware_dnsmasq.txt","Alternative list formats/antimalware_ips.txt","Alternative list formats/antimalware_lite.txt","Alternative list formats/antimalware_pure_hosts.txt"]
 checksums = {}
 for listi in lists:
   checksums[listi] = sha256(open(listi,"rb").read()).hexdigest()
-for root,dirs,files in os.walk("Alternative list formats"):
-  for file in files:
-    checksums[os.path.join(root,file)] = sha256(open(os.path.join(root,file),"rb").read()).hexdigest()
 
 checksumf = open("checksums.txt","w")
 for item in checksums:
