e68dca49919dd88915ef05e3a59728bb4f92652b
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-14 17:51:44 -0500

    Hopefully make it faster

--- scripts/checksum.py
+++ scripts/checksum.py
@@ -1,13 +1,11 @@
 """Generate checksums for various lists"""
 from hashlib import sha256
 import os
-lists = ["antimalware.txt","clickbait.txt","annoyances.txt","antitypo.txt","anti-redirectors.txt","duckduckgo-clean-up.txt","special_lists/google-safe-browsing-reverse-engineered.txt","special_lists/anti-malware-ubo-extension.txt","Alternative list formats/antimalware_domains.txt","Alternative list formats/antimalware_hosts.txt","Alternative list formats/antimalware_abp.txt","Alternative list formats/antimalware_adguard_app.txt","Alternative list formats/antimalware_adguard_home.txt","Alternative list formats/antimalware_dnsmasq.txt","Alternative list formats/antimalware_ips.txt","Alternative list formats/antimalware_lite.txt","Alternative list formats/antimalware_pure_hosts.txt"]
+lists = ["antimalware.txt","clickbait.txt","antitypo.txt","anti-redirectors.txt","special_lists/google-safe-browsing-reverse-engineered.txt","special_lists/anti-malware-ubo-extension.txt","Alternative list formats/antimalware_domains.txt","Alternative list formats/antimalware_hosts.txt","Alternative list formats/antimalware_abp.txt","Alternative list formats/antimalware_adguard_app.txt","Alternative list formats/antimalware_adguard_home.txt","Alternative list formats/antimalware_dnsmasq.txt","Alternative list formats/antimalware_ips.txt","Alternative list formats/antimalware_lite.txt","Alternative list formats/antimalware_pure_hosts.txt"]
 checksums = {}
-for listi in lists:
-  checksums[listi] = sha256(open(listi,"rb").read()).hexdigest()
-
 checksumf = open("checksums.txt","w")
-for item in checksums:
-  checksumf.write("{}: {}\n".format(item,checksums[item]))
+for listi in lists:
+  chk = sha256(open(listi,"rb").read()).hexdigest()
+  checksumf.write("{}: {}\n".format(listi,chk))
 
 checksumf.close()
