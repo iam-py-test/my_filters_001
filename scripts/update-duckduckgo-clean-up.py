1305769f5765abf8a668149f86ff4d9734c29e5b
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-10-25 19:03:58 -0400

    Update DuckDuckGo cleanup

--- scripts/update-duckduckgo-clean-up.py
+++ scripts/update-duckduckgo-clean-up.py
@@ -10,11 +10,20 @@ template = open("duckduckgo-clean-up.template")
 endfile = open("duckduckgo-clean-up.txt","w")
 maldomains = open("Alternative list formats/antimalware_domains.txt").read().split("\n")
 malips = open("Alternative list formats/antimalware_ips.txt").read().split("\n")
+pupdomains = open("Alternative list formats/anti-pup_domains.txt").read().split("\n")
+pupips = open("Alternative list formats/anti-pup_ips.txt").read().split("\n")
 maldomains.extend(malips)
+pupdomains.extend(pupips)
 total = ''
+totalpups = ''
 for line in maldomains:
   if line == '' or line.startswith("!"):
     continue
   total += """duckduckgo.com,3g2upl4pq6kufc4m.onion,duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion##[data-nrn="result"]:has(a[href*="://{}"])\nduckduckgo.com,3g2upl4pq6kufc4m.onion,duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion##.result:has(a[href*="://{}"])\n""".format(line,line)
-endfile.write(template.read().replace("{{auto-gen-time}}",currentdate.strftime('%d/%m/%Y')).replace("{mal}",total))
+for line in pupdomains:
+  if line == '' or line.startswith("!"):
+    continue
+  totalpups += """duckduckgo.com,3g2upl4pq6kufc4m.onion,duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion##[data-nrn="result"]:has(a[href*="://{}"])\nduckduckgo.com,3g2upl4pq6kufc4m.onion,duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion##.result:has(a[href*="://{}"])\n""".format(line,line)
+
+endfile.write(template.read().replace("{{auto-gen-time}}",currentdate.strftime('%d/%m/%Y')).replace("{mal}",total).replace("{pup}",totalpups))
 endfile.close()
