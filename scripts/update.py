5e71234b2b28131cb22e1e1d97d316e7016f6a1a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2022-12-17 13:34:32 -0500

    Whoops

--- scripts/update.py
+++ scripts/update.py
@@ -168,7 +168,7 @@ def convert_to_abp(clist,clistpath="./list.txt",include=False):
         endlist += "{}\n".format(line)
       elif line.startswith("||") and "$" in line:
         modifier = line.split("$")[1]
-        if "all" in modifier or "doc" in modifier or "important":
+        if "all" in modifier or "doc" in modifier or "important" in modifier:
           modifier = ""
         else:
           modifier = "${}".format(modifier)
