87e24b3c4d5404f691506e5764f1ea2af2c56977
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-04-21 20:14:10 -0400

    Fix tabs

--- scripts/create-lite.py
+++ scripts/create-lite.py
@@ -10,12 +10,9 @@ list1 = """[Adblock Plus 2.0]
 ! Homepage: https://github.com/iam-py-test/my_filters_001
 ! Issues url: https://github.com/iam-py-test/my_filters_001/issues
 ! GitLab issues url (not checked as often): https://gitlab.com/iam-py-test/my_filters_001/-/issues
-! Script last updated: 24/12/2022
+! Script last updated: 21/4/2023
 ! Last updated: {}
 ! Expires: 1 day
-
-! Main blocking rules
-||gdn^$document
 
 ! Domain/url blocking rules (auto-generated)
 """.format(datetime.datetime.now().strftime("%d/%m/%y"))
@@ -23,32 +20,32 @@ blockedtlds = ["gdn"]
 done_entries = []
 bannedfilters = []
 try:
-  bannedfilters += list(filter(bool,requests.get("https://raw.githubusercontent.com/iam-py-test/allowlist/main/filter.txt").text.split("\n")))
+	bannedfilters += list(filter(bool,requests.get("https://raw.githubusercontent.com/iam-py-test/allowlist/main/filter.txt").text.split("\n")))
 except:
-  pass
+	pass
 
 
 lines = open("antimalware.txt").read().split("\n")
 for line in lines:
-  if line in done_entries or line in bannedfilters:
-    continue
-  if line.startswith("||") and "/" in line:
-    list1 += line + "\n"
-    done_entries.append(line)
-    continue
-  if line.startswith("!") or line == "" or line.startswith("#") or "[Adblock Plus 2.0]" in line:
-    continue
-  else:
-    try:
-      tld = psl.publicsuffix(line.split("$")[0])
-      if tld in blockedtlds:
-        continue
-      else:
-        list1 += line + "\n"
-        done_entries.append(line)
-    except:
-      list1 += line + "\n"
-      done_entries.append(line)
+	if line in done_entries or line in bannedfilters:
+		continue
+	if line.startswith("||") and "/" in line:
+		list1 += line + "\n"
+		done_entries.append(line)
+		continue
+	if line.startswith("!") or line == "" or line.startswith("#") or "[Adblock Plus 2.0]" in line:
+		continue
+	else:
+		try:
+			tld = psl.publicsuffix(line.split("$")[0])
+			if tld in blockedtlds:
+				continue
+			else:
+				list1 += line + "\n"
+				done_entries.append(line)
+		except:
+			list1 += line + "\n"
+			done_entries.append(line)
 endlist = open("Alternative list formats/antimalware_lite.txt","w")
 endlist.write(list1)
 endlist.close()
