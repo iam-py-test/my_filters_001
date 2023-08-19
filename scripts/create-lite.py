34efeec96d44be9ccc75f0eeb1785f0f1739a83a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-08-19 13:18:14 -0400

    Bug fix (for real)

--- scripts/create-lite.py
+++ scripts/create-lite.py
@@ -71,7 +71,7 @@ def parse(lines):
         else:
           lcontents += line + "\n"
           done_entries.append(line)
-          all_domains.append(domain)
+          done_domains.append(domain)
           if "/" not in line and isipdomain(domain) == False:
             all_domains.append(domain)
       except Exception as err:
