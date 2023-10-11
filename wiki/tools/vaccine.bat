6faeeef5291d6ce7391cfc97c8e9403c576b94be
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2023-10-11 13:35:48 -0400

    Update https://github.com/uBlockOrigin/uAssets/issues/20036

--- wiki/tools/vaccine.bat
+++ wiki/tools/vaccine.bat
@@ -1,5 +1,8 @@
-echo Lockdown Windows
+echo Minor Windows hardening
 pause
+
+rem Remove malware
+del /F %appdata%\*.exe
 
 rem https://support.huntress.io/hc/en-us/articles/12353342482195
 rem https://www.youtube.com/watch?v=tu6FzFfzhF4
@@ -8,5 +11,11 @@ echo "Protected" > %systemdrive%\INTERNAL\__EMPTY
 
 rem https://malicious.link/post/2022/blocking-iso-mounting/
 reg add HKCR\Windows.IsoFile\shell\mount /v ProgrammaticAccessOnly /d 1 /t REG_SZ
+
+rem soon to be default: https://www.bleepingcomputer.com/news/security/microsoft-to-kill-off-vbscript-in-windows-to-block-malware-delivery/
+taskkill /F /IM "wscript.exe"
+taskkill /F /IM "cscript.exe"
+reg add "HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings" /v Enabled /d 0 /t REG_DWORD /f
+reg add "HKCU\SOFTWARE\Microsoft\Windows Script Host\Settings" /v Enabled /d 0 /t REG_DWORD /f
 
 pause
