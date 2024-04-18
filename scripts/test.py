69b736afeb4430587f9c4af8208bb80221a56ff5
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-04-18 14:30:40 -0400

    I wish I could repro on my machine

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,6 @@
 import dns, dns.resolver
 import os, sys, json, datetime, socket, random, publicsuffixlist, ssl, requests, time
+
+p = publicsuffixlist.PublicSuffixList(only_icann=True)
 resolver = dns.resolver.Resolver()
 resolver.nameservers = ["https://unfiltered.adguard-dns.com/dns-query","94.140.14.140", "8.8.8.8","1.1.1.1"]
