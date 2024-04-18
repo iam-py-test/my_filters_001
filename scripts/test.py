580867002bb420b9e5e94e6fb03e30cf30738c55
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2024-04-18 14:29:03 -0400

    Back to the drawing board

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,4 @@
 import dns, dns.resolver
 import os, sys, json, datetime, socket, random, publicsuffixlist, ssl, requests, time
 resolver = dns.resolver.Resolver()
+resolver.nameservers = ["https://unfiltered.adguard-dns.com/dns-query","94.140.14.140", "8.8.8.8","1.1.1.1"]
