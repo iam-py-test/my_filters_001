3d8f31e6e1dacd06449b7f3dd1f4c4541c7db97a
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2026-02-04 15:25:48 -0500

    Remove dead domains, fix ED, and correct four year old false positive See https://infosec.exchange/@iampytest1/116014212734738210 See also https://github.com/DandelionSprout/adfilt/issues/63#issuecomment-1230875702

--- scripts/ed.py
+++ scripts/ed.py
@@ -447,7 +447,7 @@ for e in domain_list:
                 except:
                     pass
 
-        if entry_data[e]["check_counter"] > 70 and random.choice([True,False,False,False]) == True: # will revert back to 50 soon
+        if entry_data[e]["check_counter"] > 80 and random.choice([True,False,False,False]) == True: # will revert back to 50 soon
             print(f"Checking {e}...", "previous status", entry_data[e]["check_status"], "last check", entry_data[e]["last_checked"])
             domain_is_alive = is_alive(e, True)
             if "check_history" not in entry_data[e]:
