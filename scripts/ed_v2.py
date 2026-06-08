a22572fa9b3e18786991a7053f0bfb5345d5e0ce
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2026-06-08 18:08:47 -0400

    Fix spaces

--- scripts/ed_v2.py
+++ scripts/ed_v2.py
@@ -4,10 +4,10 @@ import requests
 
 print("LOADING DATA")
 try:
-    entry_data = json.loads(open("entry_data.json", encoding="UTF-8").read())
+	entry_data = json.loads(open("entry_data.json", encoding="UTF-8").read())
 except:
-    print("FAILED TO LOAD DATA")
-    entry_data = {}
+	print("FAILED TO LOAD DATA")
+	entry_data = {}
 print("LOADED DATA")
 
 print("LOADING DOMAINS")
@@ -17,11 +17,11 @@ current_date = datetime.datetime.now().isoformat()
 entry_data["last_updated"] = current_date
 
 def get_last_commit() -> str:
-    try:
-        return requests.get("https://api.github.com/repos/iam-py-test/my_filters_001/commits").json()[0]['html_url']
-    except Exception as err:
-        print(err)
-        return None
+	try:
+		return requests.get("https://api.github.com/repos/iam-py-test/my_filters_001/commits").json()[0]['html_url']
+	except Exception as err:
+		print(err)
+		return None
 
 last_commit = get_last_commit()
 
@@ -34,19 +34,19 @@ for e in domain_list:
 			"removed_date": "",
 			"ed_version": "v2",
 			"readded": False,
-            "origin_add": "",
-            "readd": "",
+			"origin_add": "",
+			"readd": "",
 			"times_died": 0,
 			"last_commit": last_commit
 		}
 
 
 for e in entry_data:
-    if e not in domain_list and e != "last_updated" and e != "":
+	if e not in domain_list and e != "last_updated" and e != "":
 		if entry_data[e]["removed"] == False:
-            entry_data[e]["removed"] = True
-            entry_data[e]["removed_date"] = current_date
-            entry_data[e]['removed_commit'] = last_commit
+			entry_data[e]["removed"] = True
+			entry_data[e]["removed_date"] = current_date
+			entry_data[e]['removed_commit'] = last_commit
 
 entry_data_file = open("entry_data.json", 'w', encoding="UTF-8")
 entry_data_file.write(json.dumps(entry_data))
